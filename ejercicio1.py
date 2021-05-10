ca2fabe1a11a14554ec779c717a613bbfd8b9b50
Andres Sequeda Higuita <80718240+andressequeda@users.noreply.github.com>
2021-05-09 21:29:48 -0500

    Add files via upload

---
+++ ejercicio1.py
@@ -0,0 +1,3 @@
+nombre  =  str ( input ( "Ingrese su nombre:" ))
+adjetivo_calificativo  =  str ( input ( "Ingrese un adjetivo calificativo para su nombre:" ))
+print ( nombre , adjetivo_calificativo )
